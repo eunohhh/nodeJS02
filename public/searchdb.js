5ea1b0779fade860e1995675e1400aca61b907e2
Eun Oh <chorockmasil@gmail.com>
2023-06-20 11:49:52 +0900

    230620searchdb

--- public/searchdb.js
+++ public/searchdb.js
@@ -1,5 +1,5 @@
 export default
-[{"_id":0,"keywords":"screen,스크린,screenxyz,스튜디오,studio,vr,브이알","title":"스크린홈페이지","url":"https://screenxyz.net"},
+[[{"_id":0,"keywords":"screen,스크린,screenxyz,스튜디오,studio,vr,브이알","title":"스크린홈페이지","url":"https://screenxyz.net"},
 {"_id":1,"keywords":"개인정보,개인,정보,개인정보보호,privacy,policy","title":"개인정보처리방침","url":"https://screenxyz.net/privacy-policy"},
 {"_id":2,"keywords":"이용약관,terms,termsofuse,약관","title":"이용약관","url":"https://screenxyz.net/terms-of-use"},
 {"_id":3,"keywords":"3d,3d스캔,3dscan,3dvr,vr,메타포트,matterport,스캔버스,scanvas","title":"3D스캔","url":"https://screenxyz.net/3dscan"},
@@ -20,6 +20,15 @@ export default
 {"_id":18,"keywords":"src,오은,3d,조각,오브젝트,전시,온라인","title":"공간-src","url":"https://screenxyz.net/spaces/src"},
 {"_id":19,"keywords":"골프,골프연습장,뉴오션,뉴오션골프,golf,newocean,부산","title":"공간-뉴오션골프","url":"https://screenxyz.net/spaces/newoceangolf_"},
 {"_id":20,"keywords":"좋은소리,학원,스피치,부산","title":"공간-좋은소리스피치아카데미","url":"https://screenxyz.net/spaces/goodsound_"},
-{"_id":21,"keywords":"고양이,유튜브,풀숲이,풀숲,호구,전시,전시장,meowart,meow,cat,뮤아트","title":"뮤아트","url":"https://screenxyz.net"},
-{"_id":22,"keywords":"스케치박스,스케치,드로잉,sketch,drawing,picture,painting,페인팅","title":"스케치박스","url":"https://screenxyz.net"},
-{"_id":23,"keywords":"gpu,지피유,전시장,온라인,온라인공간,대여","title":"GPU","url":"https://screenxyz.net"}]
+{"_id":21,"keywords":"고양이,유튜브,풀숲이,풀숲,호구,전시,전시장,meowart,meow,cat,뮤아트","title":"뮤아트","url":"https://screenxyz.net/meowart"},
+{"_id":22,"keywords":"스케치박스,스케치,드로잉,sketch,drawing,picture,painting,페인팅","title":"스케치박스","url":"https://screenxyz.net/sketchbox"},
+{"_id":23,"keywords":"gpu,지피유,전시장,온라인,온라인공간,대여","title":"GPU","url":"https://screenxyz.net/gpu"},
+{"_id":24,"keywords":"스캔바스,3d스갠,3dscan,matterport,메타포트,360,vr,실내,실외,촬영,포토그라마트리,사진측량","title":"스캔바스","url":"https://screenxyz.net/scanvas"},
+{"_id":25,"keywords":"야외,야외촬영,광대역,3dscan,보라매공원,matterport,vr,360,공원,비행기,에어파크","title":"보라매공원에어파크","url":"https://screenxyz.net/vr/airpark"},
+{"_id":26,"keywords":"야외,야외촬영,광대역,3dscan,보라매공원,matterport,vr,360,공원,박웅진시비","title":"박웅진시비","url":"https://screenxyz.net/vr/poetmonument"},
+{"_id":27,"keywords":"야외,야외촬영,광대역,3dscan,보라매공원,matterport,vr,360,공원,보라매놀이터,보라매공원","title":"보라매공원놀이터","url":"https://screenxyz.net/vr/playground"},
+{"_id":28,"keywords":"야외,야외촬영,광대역,3dscan,보라매공원,matterport,vr,360,공원,북악팔각정,팔각정,북악,북악스카이웨이","title":"북악팔각정","url":"https://screenxyz.net/vr/bukakpavilion"},
+{"_id":29,"keywords":"야외,야외촬영,광대역,3dscan,보라매공원,matterport,vr,360,공원,북서울꿈의숲,북서울,월영지,연못","title":"월영지","url":"https://screenxyz.net/vr/moonpond"},
+{"_id":30,"keywords":"야외,야외촬영,광대역,3dscan,보라매공원,matterport,vr,360,공원,창녕위궁재사,번동,서울번동창녕위궁재사,유적,유적지","title":"서울번동창녕위궁재사","url":"https://screenxyz.net/vr/ritualhouse"},
+{"_id":31,"keywords":"가상공간,가상,메타버스,가상갤러리,갤러리,3d,webgl,full3d,3d,현대미술,조각,미니멀리즘,미니멀,생텀,sanctum","title":"Sanctum","url":"https://screenxyz.net/sanctum"},
+{"_id":32,"keywords":"가상공간,가상,메타버스,가상갤러리,갤러리,3d,webgl,full3d,3d,공간,예시,정방형,세장형,ㄷ자형,예제","title":"vas공간선택","url":"https://screenxyz.net/vas"}]]
